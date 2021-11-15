c8550bef6a4a27618c2786ecc16da53ab33a6ac9
Evgeny P <livin9000@gmail.com>
2021-11-16 04:34:56 +0500

    hover mode

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
     <Tabs>
-        <Tab height="300px" width="60%" title="First" class="w-50">
+        <Tab height="300px" width="60%" title="First">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, libero?
         </Tab>
-        <Tab height="200px" width="20%" title="Second" class="w-30">
+        <Tab height="200px" width="20%" title="Second">
             Lorem ipsum dolor sit amet.
         </Tab>
-        <Tab height="200px" width="40%" title="Third" class="w-20">
+        <Tab height="200px" width="40%" title="Third">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. A asperiores consectetur dicta ducimus, enim ex ipsum iste quibusdam recusandae veritatis.
         </Tab>
     </Tabs>
@@ -16,7 +16,6 @@
 
 import Tabs from "./components/Tabs";
 import Tab from "./components/Tab";
-import { provide } from "vue";
 
 export default {
     name: 'App',
@@ -29,16 +28,5 @@ body {
     margin: 0;
     padding: 0;
     background: gainsboro;
-}
-.w-20 {
-    width: 50%;
-}
-
-.w-30 {
-    width: 30%;
-}
-
-.w-50 {
-    width: 100%;
 }
 </style>
--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,26 +1,29 @@
 <template>
-    <div class="tags">
+    <div class="tags" @mouseleave="tabsShowed = false">
+        <div v-if="tabsShowed" class="fixed-bg" @mouseenter="tabsShowed = false"></div>
         <div class="tags-titles">
-            <div class="titles">
-                <div class="title" v-for="(tab, id) in tabs" :key="tab.props.title" @click="selectTab(tab, id)">
+            <div class="titles" @mouseleave="preventUwnatedPopUp()">
+                <div class="title" v-for="(tab, id) in tabs" :key="tab.props.title" @mouseover="selectTab(tab, id)">
                     {{ tab.props.title }}
                 </div>
             </div>
-            <div class="indicator" ref="indicator">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="10" viewBox="0 0 133 68.222">
+            <div v-show="tabsShowed" class="indicator" ref="indicator">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="8" viewBox="0 0 133 68.222">
                     <path id="Union_1" data-name="Union 1" d="M255.056,221H222l21.755-24.862.013-.015L277.212,157.9a15,15,0,0,1,22.577,0L326.125,188h0L355,221l0,0H255.056ZM355,221h0ZM355,221Z" transform="translate(-222 -152.779)" fill="#fff"/>
                 </svg>
             </div>
         </div>
-        <div class="tags-body" :style="{width: selectedWidth, height: selectedHeight}">
-            <slot/>
-        </div>
+<!--        <transition name="">-->
+            <div v-if="tabsShowed" class="tags-body" :style="{width: selectedWidth, height: selectedHeight}">
+                <slot/>
+            </div>
+<!--        </transition>-->
     </div>
 </template>
 
 <script>
 import Tab from "./Tab";
-import { ref, provide, onMounted } from 'vue'
+import { ref, provide, onMounted, watchEffect } from 'vue'
 
 export default {
     name: "Tabs",
@@ -31,8 +34,20 @@ export default {
         const selectedWidth = ref(selectedTab.value.props.width)
         const selectedHeight = ref(selectedTab.value.props.height)
         const indicator = ref(null)
+        const tabsShowed = ref(false)
+        const selectedTabTime = ref(0)
+
+        // to prevent unwanted pop-ups due to fast movement of the mouse over titles
+        const preventUwnatedPopUp = () => {
+            if (Date.now() - selectedTabTime.value < 100) {
+                tabsShowed.value = false
+            }
+        }
 
         const selectTab = (tab, id) => {
+            selectedTabTime.value = Date.now()
+
+            tabsShowed.value = true
             selectedTab.value = tab
 
             selectedWidth.value = tab.props.width
@@ -48,7 +63,10 @@ export default {
 
         provide('selectedTab', selectedTab)
 
-        return {selectedTab, tabs, selectedWidth, selectedHeight, selectTab, indicator}
+        return {
+            selectedTab, tabs, selectedWidth, selectedHeight,
+            selectTab, indicator, tabsShowed, preventUwnatedPopUp, selectedTabTime
+        }
     }
 }
 </script>
@@ -59,11 +77,18 @@ export default {
     flex-flow: column;
     align-items: center;
 }
+.fixed-bg {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+}
 .tags-titles {
-    width: 50%;
+    color: white;
+    font-weight: bold;
+    background: gray;
+    width: 20%;
     display: flex;
     flex-flow: column;
-    //align-items: center;
     position: relative;
 }
 .titles {
@@ -75,20 +100,21 @@ export default {
     width: 100%;
     padding: 20px 0;
     text-align: center;
-    cursor: pointer;
+    cursor: default;
+    z-index: 1;
 }
 .indicator {
     text-align: center;
     position: absolute;
-    margin-top: 50px;
-    margin-bottom: 5px;
-    transition: all 0.4s ease;
+    margin-top: 45px;
+    transition: all 0.25s ease;
 }
 .tags-body {
-    margin-top: 5px;
+    z-index: 1;
+    width: auto;
     background: white;
     padding: 24px;
-    transition: all 0.4s ease;
+    transition: all 0.25s ease;
     border-radius: 7px;
     box-shadow: 0 12px 25px 8px rgba(0,0,0,0.05);
 }
